afdb3dd0cfced79f86c0e5f4979a492f210c9ca4
Willem Lucas <willemlucas@Willems-MacBook-Pro.local>
2018-04-12 01:54:22 -0400

    working requests

--- FinalProj.py
+++ FinalProj.py
@@ -1,18 +1,56 @@
 import spotipy
 import secrets
 import spotipy.util as util
+import json
+from bs4 import BeautifulSoup
+import sys
 
-# CACHE_FNAME = 'cached_music.json'
-# baseurl = 'https://api.spotify.com'
-# util.prompt_for_user_token('hockeybean',scope = '' ,client_id= secrets.client_id,
-# client_secret= secrets.client_secret,redirect_uri= secrets.redirect_uri)
-
-token = util.oauth2.SpotifyClientCredentials(client_id= secrets.client_id, client_secret= secrets.client_secret)
-
+token = util.oauth2.SpotifyClientCredentials(client_id=secrets.client_id,
+client_secret=secrets.client_secret)
 cache_token = token.get_access_token()
 spotify = spotipy.Spotify(cache_token)
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+
+CACHE_FNAME = 'cached_music.json'
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    SPOTIFY_CACHED_DICT = json.loads(cache_contents)
+    cache_file.close()
+except:
+    SPOTIFY_CACHED_DICT = {}
+
+def get_data_using_cache(field='artist:',artistName = 'Radiohead',
+return_type='track'):
+    spotify_dict = (spotify.search(q=field + str(artistName),
+    type=return_type))
+    key = ['tracks']['href']
+    #check what Key is here ^
+    if key in SPOTIFY_CACHED_DICT:
+        return SPOTIFY_CACHED_DICT[unique_indent]
+    else:
+        response = spotify.search(q=field + str(artistName),
+        type=return_type)
+        SPOTIFY_CACHED_DICT[unique_indent] = response
+        fref = open('cached_music.json','w')
+        dumped_data = json.dumps(SPOTIFY_CACHED_DICT)
+        fref.write(dumped_data)
+        fref.close()
+        return SPOTIFY_CACHED_DICT[unique_indent]
+
+x = ''
+while x != 'quit':
+    x = str(input('What artist would you like to search?'))
+    get_data_using_cache(artistName = x)
+    spot_list = spotify.search(q='artist'+ str(x))
+    print(refined_list)
+
+tracks = []
+
+spot_list['external urls']['name']
+
+
+
 results = spotify.artist_albums(birdy_uri, album_type='album')
 albums = results['items']
 while results['next']:
@@ -24,15 +62,37 @@ for album in albums:
 
 
 
-# def get_data_using_cache(baseurl):
-#     unique_indent = baseurl
-#     if baseurl in CACHE_DICT:
-#         return CACHE_DICT[baseurl]
+
+# Part1 Oauth page server requests
+# baseurl = 'https://api.spotify.com'
+# util.prompt_for_user_token('hockeybean',scope = '' ,client_id= secrets.client_id,
+# client_secret= secrets.client_secret,redirect_uri= secrets.redirect_uri)
+# token = util.oauth2.SpotifyClientCredentials(client_id= secrets.client_id, client_secret= secrets.client_secret)
+# cache_token = token.get_access_token()
+# spotify = spotipy.Spotify(cache_token)
+#
+#
+# try:
+#     cache_file = open(CACHE_FNAME, 'r')
+#     cache_contents = cache_file.read()
+#     SPOTIFY_CACHED_DICT = json.loads(cache_contents)
+#     cache_file.close()
+# except:
+#     SPOTIFY_CACHED_DICT = {}
+#
+# def process_json_dict(self, json_dict):
+#
+#
+# def get_spotify_cache(baseurl, params):
+#     unique_ident = oauth.get(baseurl, params=params).url
+#     spotify.artist_albums(birdy_uri, album_type='album')
+#     if unique_ident in SPOTIFY_CACHED_DICT:
+#         return TWITTER_CACHE_DICT[unique_ident]
 #     else:
-#         response = requests.get(baseurl)
-#         CACHE_DICT[unique_indent] = response.text
-#         fref = open('cached_music.json', 'w')
-#         dumped_data = json.dumps(CACHE_DICT)
-#         fref.write(dumped_data)
-#         fref.close()
-#         return CACHE_DICT[unique_indent]
+#         resp = oauth.get(baseurl, params=params)
+#         SPOTIFY_CACHED_DICT[unique_ident] = json.loads(resp.text)
+#         f = open('twitter_cache.json', 'w')
+#         dumped_json = json.dumps(SPOTIFY_CACHED_DICT)
+#         f.write(dumped_json)
+#         f.close()
+#         return SPOTIFY_CACHED_DICT[unique_ident]
